0db0275090564fac548397d175bb70064db4ddb4
Luis Carlin <luiscarlin@gmail.com>
2018-09-06 06:56:50 -0400

    Continue with pattern

---
+++ src/main/java/abstract_factory/UFOEnemyShipBuilding.java
@@ -0,0 +1,8 @@
+package abstract_factory;
+
+public class UFOEnemyShipBuilding extends EnemyShipBuilding {
+    @Override
+    protected EnemyShip makeEnemyShip(String typeOfShip) {
+        return null;
+    }
+}
